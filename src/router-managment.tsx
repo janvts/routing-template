2bac646c45f7bf48836c924d67b0e7804e7693bb
janvts <1922656@uni-wuppertal.de>
2023-06-07 15:59:31 +0200

    updated readme

--- src/router-managment.tsx
+++ src/router-managment.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './home.css';
 import './index.css';
-import {createBrowserRouter, Navigate, createHashRouter} from "react-router-dom"
+import {createBrowserRouter, Navigate} from "react-router-dom"
 import {Home} from "./home";
 import ErrorPage from "./components/error-page";
 import {Component1} from "./components/component1";
 import {Component2} from "./components/component2";
 
 const homepageUrl = process.env.PUBLIC_URL;
-const router = createHashRouter([
+const router = createBrowserRouter([
 
     {
         path: `${homepageUrl}`,
