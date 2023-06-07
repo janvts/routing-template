152db10c18513789e1967a14064421fd22dbd4f2
janvts <1922656@uni-wuppertal.de>
2023-06-07 14:48:17 +0200

    added routing route for routing-template extension

--- src/router-managment.tsx
+++ src/router-managment.tsx
@@ -16,7 +16,7 @@ const router = createBrowserRouter([
         children: [
             {
                 path: 'routing-template',
-                element: <Navigate to="/" replace/>
+                element: <Home/>
             },
             {
                 path: "component1",
