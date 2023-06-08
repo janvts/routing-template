c3878d22d056622c5098ed52ee721e8dedf1ad0f
janvts <1922656@uni-wuppertal.de>
2023-06-08 16:03:39 +0200

    updated basename

--- src/router-managment.tsx
+++ src/router-managment.tsx
@@ -8,6 +8,7 @@ import {Component1} from "./components/component1";
 import {Component2} from "./components/component2";
 
 const homepageUrl = process.env.PUBLIC_URL;
+
 const router = createBrowserRouter([
 
     {
